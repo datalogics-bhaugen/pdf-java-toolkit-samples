e9d682f77e72478a27570f19580c0a7c11a6daff
Brandon Haugen <bhaugen@brandons-mbp-2.dlogics.com>
2016-06-17 09:05:42 -0500

    Add start of FormTypeDetector

---
+++ src/main/java/com/datalogics/pdf/samples/forms/FormTypeDetector.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2015 Datalogics, Inc.
+ */
+
+package com.datalogics.pdf.samples.forms;
+
+/**
+ * This sample demonstrates how to detect the type of form that is in a PDF. It will recursively search through the path
+ * supplied as an argument, defaulting to the directory where the sample is run from, for all files that are of type
+ * 'PDF' and count the number of each type of form that it finds.
+ */
+public class FormTypeDetector {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
