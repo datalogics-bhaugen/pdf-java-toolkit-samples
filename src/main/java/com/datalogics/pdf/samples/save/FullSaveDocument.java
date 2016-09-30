5f54a42504f78ccbff7de5c1f8981acd4ccc1f91
Brandon Haugen <bhaugen@brandons-mbp-2.dlogics.com>
2016-09-30 09:10:42 -0500

    Add example of full save for documents

---
+++ src/main/java/com/datalogics/pdf/samples/save/FullSaveDocument.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 Datalogics, Inc.
+ */
+
+package com.datalogics.pdf.samples.save;
+
+import com.adobe.internal.io.ByteWriter;
+import com.adobe.pdfjt.core.license.LicenseManager;
+import com.adobe.pdfjt.pdf.document.PDFDocument;
+import com.adobe.pdfjt.pdf.document.PDFSaveFullOptions;
+
+import com.datalogics.pdf.samples.util.DocumentUtils;
+import com.datalogics.pdf.samples.util.IoUtils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ */
+public class FullSaveDocument {
+    final static String INPUT_PDF = "pdfjavatoolkit-ds.pdf";
+    final static String OUTPUT_PDF = "FullSave.pdf";
+
+    /**
+     * @param args
+     * @throws MalformedURLException
+     */
+    public static void main(final String[] args) throws MalformedURLException {
+        // If you are using an evaluation version of the product (License Managed, or LM), set the path to where PDFJT
+        // can find the license file.
+        //
+        // If you are not using an evaluation version of the product you can ignore or remove this code.
+        LicenseManager.setLicensePath(".");
+        URL inputUrl = null;
+        URL outputUrl = null;
+        if (args.length == 2) {
+            inputUrl = IoUtils.createUrlFromPath(args[0]);
+            outputUrl = IoUtils.createUrlFromPath(args[1]);
+        } else {
+            inputUrl = FullSaveDocument.class.getResource(INPUT_PDF);
+            outputUrl = IoUtils.createUrlFromPath(OUTPUT_PDF);
+        }
+
+        fullSaveDocument(inputUrl, outputUrl);
+    }
+
+    /**
+     * @param inputUrl
+     * @param outputUrl
+     */
+    private static void fullSaveDocument(final URL inputUrl, final URL outputUrl) {
+        PDFDocument pdfDoc = null;
+        ByteWriter writer = null;
+
+        try {
+            pdfDoc = DocumentUtils.openPdfDocument(inputUrl);
+
+            writer = IoUtils.newByteWriter(outputUrl);
+
+            final PDFSaveFullOptions options = PDFSaveFullOptions.newInstance();
+
+            pdfDoc.saveAndClose(writer, options);
+        } catch (final Exception e) {
+
+        }
+    }
+}
